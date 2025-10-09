46b1c8d58ff00ec7d077685a3a792e7b0d387f03
Devsdecero <deceroapros@gmail.com>
2025-10-09 19:51:17 +0200

    OG banner and rich snippets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,22 +4,22 @@ import { Toaster } from '@/components/ui/toaster';
 import { ThemeProvider } from '@/components/ThemeProvider';
 
 const appURL = 'https://python-playground.vercel.app'; 
-const ogImageURL = `${appURL}/logo_devs.png`
+const ogImageURL = `${appURL}/og-pp-banner.png`
 
 export const metadata: Metadata = {
   title: 'Python Playground | DevsdeCero',
-  description: 'Una aplicación web segura y robusta para aprender con ejercicios en Python, que utiliza Pyodide para ejecutar código en un entorno aislado (sandbox) del navegador.',
+  description: 'Una aplicación web segura, divertida e interactiva para aprender con ejercicios en Python. ¡Empieza ya!',
   
   openGraph: {
     title: 'Python Playground | DevsdeCero',
-    description: 'Una aplicación web segura y robusta para aprender con ejercicios en Python, que utiliza Pyodide para ejecutar código en un entorno aislado (sandbox) del navegador.',
+    description: 'Una aplicación web segura, divertida e interactiva para aprender con ejercicios en Python. ¡Empieza ya!',
     url: appURL,
     siteName: 'Python Playground | DevsdeCero',
     images: [
       {
         url: ogImageURL,
-        width: 512,
-        height: 512,
+        width: 1200,
+        height: 630,
         alt: 'Logo DevsdeCero',
       },
     ],
@@ -29,8 +29,8 @@ export const metadata: Metadata = {
 
   twitter: {
     card: 'summary_large_image',
-    title: 'Python Playground',
-    description: 'Aprende Python de forma segura y directa, sin necesidad de instalación.', 
+    title: 'Python Playground | DevsdeCero',
+    description: 'Una aplicación web segura, divertida e interactiva para aprender con ejercicios en Python. ¡Empieza ya!', 
     images: [ogImageURL],
   },
 }
@@ -48,6 +48,17 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/pyodide/v0.26.1/full/pyodide.js"></script>
+        <script 
+          type="application/ld+json" 
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebPage", 
+              "name": "Python Playground | DevsdeCero",
+              "description": "Una aplicación web segura, divertida e interactiva para aprender con ejercicios en Python. ¡Empieza ya!",
+              "url": "https://python-playground-theta.vercel.app/", 
+            })
+          }}/>
       </head>
       <body>
         <ThemeProvider
